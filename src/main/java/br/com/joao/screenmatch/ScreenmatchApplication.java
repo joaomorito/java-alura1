4827510c29bc59459d83d9df6a2e0040d13449cd
joaomorito <joaomorito04@gmail.com>
2025-01-16 14:56:56 -0300

    Modelando dados

--- src/main/java/br/com/joao/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/joao/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.joao.screenmatch;
 
+import br.com.joao.screenmatch.model.DadosSerie;
 import br.com.joao.screenmatch.services.ConsumoApi;
+import br.com.joao.screenmatch.services.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,5 +21,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		//System.out.println(json);
 		//json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
 		System.out.println(json);
+		ConverteDados conversor =new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
