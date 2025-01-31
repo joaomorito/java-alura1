53354b146534867a772f4c27b26d21a7d0d9c6b9
joaomorito <joaomorito04@gmail.com>
2025-01-31 14:46:40 -0300

    Trabalhando na coleção de dados

--- src/main/java/br/com/joao/screenmatch/principal/Principal.java
+++ src/main/java/br/com/joao/screenmatch/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.joao.screenmatch.principal;
 
+import br.com.joao.screenmatch.model.DadosEpisodio;
 import br.com.joao.screenmatch.model.DadosSerie;
 import br.com.joao.screenmatch.model.DadosTemporada;
 import br.com.joao.screenmatch.services.ConsumoApi;
@@ -32,6 +33,16 @@ public class Principal {
             DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
             temporadas.add(dadosTemporada);
         }
+        temporadas.forEach(System.out::println);
+
+//        for(int i = 0; i < dados.totalTemporadas(); i++){
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for(int j = 0; j < episodiosTemporada.size(); j++){
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
         temporadas.forEach(System.out::println);
     }
 }
